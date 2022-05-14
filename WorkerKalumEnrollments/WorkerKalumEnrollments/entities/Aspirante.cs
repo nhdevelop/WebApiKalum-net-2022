8e62c523563eb2d4648eac25e04843469752ff57
nhdevelop <87239461+nhdevelop@users.noreply.github.com>
2022-05-14 09:56:51 -0600

    Init project

---
+++ WorkerKalumEnrollments/WorkerKalumEnrollments/entities/Aspirante.cs
@@ -0,0 +1,17 @@
+namespace WorkerKalumEnrollments.entities
+{
+public class aspirantes {
+    public string Noexpediene {get; get;}
+    public string CUT {get; get;}
+    public string Apellidos {get; get;}
+    public string Nombres {get; get;}
+    public string Direccion {get; get;}
+    public string Telefono {get; get;}
+    public string Email {get; get;}
+    public string Estatus {get; get;}
+    public DateTime Fechaexamenadmision {get; get;}
+}
+}
+
+
+
